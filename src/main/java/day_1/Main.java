382b977f80217fd1244b41a7ff6958384f599ccc
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2018-12-01 16:59:01 +0100

    Added 1_2 solution

---
+++ src/main/java/day_1/Main.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Day_1_1 day_1_1 = new Day_1_1();
+        Day_1_2 day_1_2 = new Day_1_2();
+
+        System.out.println("1: " + day_1_1.calculateFrequency());
+        System.out.println("2: " + day_1_2.firstRepeatedFrequency());
+    }
+}
