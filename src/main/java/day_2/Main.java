8b002e2d77e96b2dd7a203a82ebab813fbdd3b68
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2018-12-02 07:18:05 +0100

    Added main for 2 day solutions

---
+++ src/main/java/day_2/Main.java
@@ -0,0 +1,11 @@
+package day_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Day_2_1 day_2_1 = new Day_2_1();
+        Day_2_2 day_2_2 = new Day_2_2();
+
+        System.out.println(day_2_1.calculateChecksum());
+        System.out.println(day_2_2.getCommon());
+    }
+}
