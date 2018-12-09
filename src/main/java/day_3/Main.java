ec1254201ecedcd0c878007f3cb4f18080af5f5e
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2018-12-09 11:43:04 +0100

    Added main for 3 day solutions

---
+++ src/main/java/day_3/Main.java
@@ -0,0 +1,12 @@
+package day_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Day_3_1 day_3_1 = new Day_3_1();
+        Day_3_2 day_3_2 = new Day_3_2();
+
+        System.out.println(day_3_1.getCommonFabricSquareInches());
+        System.out.println(day_3_2.getIdOfNotOverleapedClaim());
+    }
+}
